d276121b9f85b69ff282d3ea470b12033fbcefd0
Etienne <etienne.doux@gmail.com>
2022-09-03 10:22:01 +0200

    FIX: quit loop when path is found, custom path first (#191)
    Co-authored-by: hlky <106811348+hlky@users.noreply.github.com>

--- webuildm.cmd
+++ webuildm.cmd
@@ -3,30 +3,31 @@
 set conda_env_name=ldm
 
 :: Put the path to conda directory after "=" sign if it's installed at non-standard path:
-set cutom_conda_path=
+set custom_conda_path=
+
+IF NOT "%custom_conda_path%"=="" (
+  set paths=%custom_conda_path%;%paths%
+)
 
 set paths=%ProgramData%\miniconda3
 set paths=%paths%;%USERPROFILE%\miniconda3
 set paths=%paths%;%ProgramData%\anaconda3
 set paths=%paths%;%USERPROFILE%\anaconda3
 
-IF NOT "%cutom_conda_path%"=="" (
-  set paths=%cutom_conda_path%;%paths%
-)
-
-for %%a in (%paths%) do ( 
+for %%a in (%paths%) do (
  if EXIST "%%a\Scripts\activate.bat" (
     SET CONDA_PATH=%%a
+    echo anaconda3/miniconda3 detected in %%a
+    goto :foundPath
  )
 )
 
 IF "%CONDA_PATH%"=="" (
   echo anaconda3/miniconda3 not found. Install from here https://docs.conda.io/en/latest/miniconda.html
-  exit /b 1 
-) else (
-  echo anaconda3/miniconda3 detected in %CONDA_PATH%
+  exit /b 1
 )
 
+:foundPath
 call "%CONDA_PATH%\Scripts\activate.bat"
 call conda env create -n "%conda_env_name%" -f environment.yaml
 call conda env update -n "%conda_env_name%" --file environment.yaml --prune
